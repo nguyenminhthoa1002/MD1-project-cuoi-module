2b9bcbe59231f57ca97699b53c0c0e478de86fa6
nguyenminhthoa1002 <minhthoa100299@gmail.com>
2022-09-22 08:07:41 +0700

    lan-2

--- register/Users.js
+++ register/Users.js
@@ -12,4 +12,35 @@ class Users {
         this.email = email;
         this.password = password;
     }
+
+    // setId(id){
+    //     this.id = id;
+    // }
+    // getId() {
+    //     return this.id;
+    // }
+    // setName(name){
+    //     this.name = name;
+    // }
+    // getName(){
+    //     return this.name;
+    // }
+    // setUsername(username){
+    //     this.username=username;
+    // }
+    // getUsername(){
+    //     return this.username;
+    // }
+    // setEmail(email){
+    //     this.email = email;
+    // }
+    // getEmail(){
+    //     return this.email;
+    // }
+    // setPassword(password){
+    //     this.password = password;
+    // }
+    // getPassword(){
+    //     return this.password;
+    // }
 }
